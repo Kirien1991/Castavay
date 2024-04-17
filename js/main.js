0e8a5259a2908167eca8377ff0a8f93dcae76d8d
Yauheni Aliakseyenko <yauheni.aliakseyenko@neklo.com>
2024-04-17 10:01:38 +0300

    update

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("form") 
+console.log(form)
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    console.dir(event)
+    console.dir(form)
+    const name = form[0]
+    const email = form[1]
+    console.dir(name.value)
+    console.dir(email.value)
+    const nameValue = name.value
+    const emailValue = email.value
+    alert(nameValue + " - " + emailValue)
+})
