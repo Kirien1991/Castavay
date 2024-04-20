a86c718a599b2c2df3b4f0f975daedb80e5773a8
Kirien1991 <162041026+Kirien1991@users.noreply.github.com>
2024-04-20 20:10:12 +0300

    Test

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
-const form = document.getElementById("form") 
-console.log(form)
+//form//
+const form = document.getElementById("form");
+console.log(form);
 
-form.addEventListener("submit", function(event){
-    event.preventDefault()
-    console.dir(event)
-    console.dir(form)
-    const name = form[0]
-    const email = form[1]
-    console.dir(name.value)
-    console.dir(email.value)
-    const nameValue = name.value
-    const emailValue = email.value
-    alert(nameValue + " - " + emailValue)
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.dir(event);
+  console.dir(form);
+  const name = form[0];
+  const email = form[1];
+  console.dir(name.value);
+  console.dir(email.value);
+  const nameValue = name.value;
+  const emailValue = email.value;
+  alert(nameValue + " - " + emailValue);
+});
+
+//rating//
