d8f58d024520962384c838315e6edcd339b80e89
Kirien1991 <162041026+Kirien1991@users.noreply.github.com>
2024-04-28 21:43:12 +0300

    End

--- js/main.js
+++ js/main.js
@@ -16,3 +16,12 @@ form.addEventListener("submit", function (event) {
 });
 
 //rating//
+const ratingItemsList = document.querySelectorAll(".rating__item");
+const ratingItemsArray = Array.prototype.slice.call(ratingItemsList);
+
+ratingItemsArray.forEach((item) =>
+  item.addEventListener(
+    "click",
+    () => (item.parentNode.dataset.totalValue = item.dataset.itemValue)
+  )
+);
